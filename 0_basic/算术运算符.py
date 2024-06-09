0a0492a736793ec033d7ec514216c9635bddb894
fu-jw <contact@fu-jw.com>
2024-06-09 10:53:46 +0800

    添加算术运算符

---
+++ 0_basic/算术运算符.py
@@ -0,0 +1,21 @@
+a = 21
+b = 10
+
+print("a + b = ", a + b)
+
+print("a - b = ", a - b)
+
+print("a * b = ", a * b)
+
+print("a / b = ", a / b)
+
+print("a % b = ", a % b)
+
+# 修改变量 a 、b
+a = 2
+b = 3
+print("a ** b = ", a ** b)  # 幂
+
+a = 10
+b = 4
+print("a // b = ", a // b)  # 向下取整
